13daaefc5bd0fc531f27a3c08583dcde39703c63
Shreyashyadav0 <shreyashyadav0@gmail.com>
2023-01-23 10:04:34 +0530

    all done

---
+++ index.js
@@ -0,0 +1,51 @@
+// const express = require ("express");
+// const logger = require("morgan");
+// const app = express();
+// const RedisClient = require("./config/connectRedis");
+// const scheduleRoutes = require("./routes/scheduleRoutes");
+// require("./services/scheduler");
+
+// const PORT = 1338;
+// app.use (express.json());
+// app.use(logger("dev"));
+
+// app.listen(PORT, () => {
+//     console.log("server is running");
+//     RedisClient.connect()
+//     .then( () => {
+//         console.log("Connected to redis")
+//     })
+//     .catch((e) => {
+//      console.log("Connexted to redis")
+//     })
+// }) 
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const app = express();
+require("./services/scheduler");
+const scheduleRoutes = require("./routes/scheduleRoutes");
+const RedisClient = require("./config/connectRedis");
+
+const PORT = 1338;
+
+// Middleware
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+app.use(logger("dev"));
+
+// Routes
+app.get("/", (_req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+app.use("/api/v1/schedule", scheduleRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  RedisClient.connect()
+    .then(console.log("Connected to redis"))
+    .catch((err) => {
+      console.log("Error " + err);
+    });
+});
